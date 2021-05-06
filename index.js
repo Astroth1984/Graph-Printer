6df89f4a76c11a670d03519ef36fc3fa92b33ee9
Astroth1984 <elmounir.othmane666@gmail.com>
2021-05-06 08:52:06 +0200

    graph printer

---
+++ index.js
@@ -0,0 +1,20 @@
+ const jsonfile = require('jsonfile');
+ const moment = require('moment');
+ const random = require('random');
+ const simpleGit = require('simple-git');
+ const FILE_PATH = './data.json';
+ const makeCommit = n => {
+     if (n === 0) return simpleGit().push();
+     const x = random.int(0, 38);
+     const y = random.int(0, 12);
+     const DATE = moment().subtract(1, 'y').add(1, 'd')
+         .add(x, 'w').add(y, 'd').format();
+     const data = {
+         data: DATE
+     }
+     jsonfile.writeFile(FILE_PATH, data, () => {
+         simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE },
+             makeCommit.bind(this, --n));
+     });
+ }
+ makeCommit(60);
